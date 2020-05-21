cbf7c66bb5fc37636f494e413383d7b1e9ae370f
Malachi Ivey <zarekbusiness@gmail.com>
2020-05-21 12:51:39 -0400

    Added logic for phone number and email parsing functionality.

--- src/book_work/phoneAndEmail.py
+++ src/book_work/phoneAndEmail.py
@@ -19,6 +19,20 @@ emailRegex = re.compile(r'''(
     (\.[a-zA-Z]{2,4})      # dot-something
     )''', re.VERBOSE)
 
-# TODO: Find matches in clipboard text.
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+    for groups in emailRegex.findall(text):
+        matches.append(groups[0])
 
-# TODO: Copy results to the clipboard.
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
